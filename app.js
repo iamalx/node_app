28108fdaf57b6da6bbf24fee4c5303f413d9b3e8
Alex <reyesgx11@gmail.com>
2021-02-05 12:29:35 -0800

    parse repsonse

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const fs = require('fs')
 
 //  REDIRECTING REQUEST 
     
-const server = http.createServer((req, res) => {
+/* const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers)
     
     const url = req.url
@@ -91,6 +91,53 @@ const server = http.createServer((req, res) => {
        res.setHeader('Location', '/')
        return res.end()
     }
+    // set headers as a response
+    res.setHeader('Content-Type', 'text/html')
+
+    // responce with html code 
+    res.write('<html>')
+    res.write('<head><title>Node app</title></head>')
+    res.write('<body>Hello World!</body>')
+    res.write('</html>')
+
+    // end write responce 
+    res.end()
+}) */
+
+// PARSING REQUEST BODIES
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url, req.method, req.headers)
+    console.log(1)
+    const url = req.url
+    const method = req.method
+    if(url === '/') {
+        console.log(req.url, '/...')
+        res.write('<html>')
+        res.write('<head><title>Node app - Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit"></button></form></body>')
+        res.write('</html>')    
+        return res.end()
+    }
+ 
+    if(url === '/message' &&  method === 'POST') {
+        console.log(req.url, '/message...')
+        const data = []
+        req.on('data', (chuck) => {
+           data.push(chuck) 
+        })
+        return req.on('end', () => {
+            const parseData = Buffer.concat(data).toString()
+            const messageInput = parseData.split('=')[1]
+            fs.writeFileSync('message.txt', messageInput) 
+
+            // res.writeHead(302, {headerObject}) // 1 option instead of setHEader() and statusCode 
+            res.statusCode = 302
+            res.setHeader('Location', '/')
+            return res.end()
+        })
+    }
+    // console.log('ssss')
     // set headers as a response
     res.setHeader('Content-Type', 'text/html')
 
