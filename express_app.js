fdff39cc9ec1c689c8930958981e09f8f5b69364
Alex <reyesgx11@gmail.com>
2021-02-13 23:17:00 -0800

    epress changes

--- express_app.js
+++ express_app.js
@@ -4,20 +4,28 @@ const  express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log(next)
-    next() //allows the request to continue to the next middlewear
+app.use('/',(req, res, next) => {
+    console.log(next, 'first')
+    // next() //allows the request to continue to the next middlewear
+    res.send(`<h1>Hello World!!!!!!</h1>`)
 })
 
-app.use((req, res, next) => {
-    console.log(next)
+app.use('/product-page',(req, res, next) => {
+    console.log(next, 'next')
     // res.setHeader('') // replace res.
     res.send(`<h1>Hello World!</h1>`) //content type is set to html/txt as header 
 
 })
 
+app.use('/',(req, res, next) => {
+    console.log(next, 'last')
+    // res.setHeader('') // replace res.
+    // res.send(`<h1>Hello World!</h1>`) //content type is set to html/txt as header 
+
+})
 // const server = http.createServer(app) // not needed in Express
 app.listen(4000)
 
 
 
+// dbncjdsbvkhsv
